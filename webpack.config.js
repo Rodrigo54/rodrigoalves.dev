e1fc596884d3c6935a13cfd40cc1a44637cb39a1
Rodrigo Alves Mesquita <rodrigo54mix@gmail.com>
2019-01-13 18:12:45 -0300

    update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,70 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
 
-module.exports = [{
-  entry: ['./src/scss/app.scss', './src/js/app.js'],
-  output: {
-    filename: 'static/js/bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'static/css/bundle.css',
-            },
+module.exports = [
+  {
+    entry: ['./src/scss/app.scss', './src/js/app.js'],
+    output: {
+      path: path.resolve(__dirname, 'static'),
+      filename: 'js/bundle.js'
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          extractComments: 'all',
+        }),
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorPluginOptions: {
+            preset: ['default', { discardComments: { removeAll: true } }],
           },
-          { loader: 'extract-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              includePaths: ['./node_modules']
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              minimize: true,
-              includePaths: ['./node_modules']
-            }
+        })
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
           }
-        ]
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-          plugins: ['transform-object-assign']
         },
-      }
+        {
+          test: /\.s[c|a]ss$/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: MiniCssExtractPlugin.loader },
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                minimize: true,
+                includePaths: ['./node_modules']
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'src/img/**/*', to: 'img/', flatten: true }
+      ]),
+      new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
+      new MiniCssExtractPlugin({
+        filename: 'css/bundle.css'
+      })
     ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      { from: 'src/img/**/*', to: 'static/img/', flatten: true },
-    ]),
-    new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
-  ]
-}];
+  }
+];
