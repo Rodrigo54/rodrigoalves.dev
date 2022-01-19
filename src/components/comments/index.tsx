5195c9e1e6ee89d32f4cc10e078ce105317a1b88
Rodrigo Alves Mesquita <rodrigo54mix@gmail.com>
2022-01-19 19:44:37 -0300

    FIX Comments

--- src/components/comments/index.tsx
+++ src/components/comments/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DiscussionEmbed } from 'disqus-react';
 
 import * as S from './styles';
+import { BehaviorSubject } from 'rxjs';
+
+export const disqusData = new BehaviorSubject<{
+  url?: string;
+  title?: string;
+  identifier?: string;
+  shortname: string;
+  language: string;
+}>({
+  shortname: 'rodrigo-io',
+  language: 'pt-BR',
+});
 
 type Props = {
   title: string;
@@ -10,20 +22,31 @@ type Props = {
 };
 
 const Comments: React.FC<Props> = ({ slug, title, identifier }) => {
-  const url = `https://rodrigoalves.dev/blog/${slug}`;
+  const [disqusConfig, setDisqusConfig] = useState({});
+
+  useEffect(() => {
+    if (slug) {
+      const url = `https://rodrigoalves.dev/blog/${slug}`;
+      setDisqusConfig({
+        url,
+        title,
+        identifier: `${identifier}`,
+        language: 'pt-BR',
+      });
+      disqusData.next({
+        url,
+        title,
+        identifier: `${identifier}`,
+        shortname: 'rodrigo-io',
+        language: 'pt-BR',
+      });
+    }
+  }, [slug, identifier, title]);
 
   return (
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
-      <DiscussionEmbed
-        shortname="rodrigo-io"
-        config={{
-          url,
-          title,
-          identifier: `${identifier}`,
-          language: 'pt-BR',
-        }}
-      />
+      <DiscussionEmbed shortname="rodrigo-io" config={disqusConfig} />
     </S.CommentsWrapper>
   );
 };
