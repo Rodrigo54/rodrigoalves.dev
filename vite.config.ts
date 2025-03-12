258288e4ec04b9f47ea01d62852faf533d912d71
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-03-12 17:41:38 -0300

    ✨ feat: inicializar projeto com configuração do Analog JS

---
+++ vite.config.ts
@@ -0,0 +1,64 @@
+/// <reference types="vitest" />
+
+import { defineConfig, PluginOption } from 'vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import analog from '@analogjs/platform';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  publicDir: 'src/assets',
+  build: {
+    target: ['es2020'],
+  },
+  resolve: {
+    mainFields: ['module'],
+  },
+  plugins: [
+    analog({
+      liveReload: false,
+      static: true,
+      content: {
+        highlighter: 'shiki',
+        shikiOptions: {
+          highlighter: {
+            additionalLangs: ['shellscript'],
+          },
+        },
+      },
+      prerender: {
+        routes: async () => [
+          '/blog',
+          {
+            contentDir: 'src/content',
+            transform: (file) => {
+              // do not include files marked as draft in frontmatter
+              if (file.attributes['draft']) {
+                return false;
+              }
+              // use the slug from frontmatter if defined, otherwise use the files basename
+              const slug = file.attributes['slug'] || file.name;
+              return `/blog/${slug}`;
+            }
+          }
+        ],
+      },
+      nitro: {
+        prerender: {
+          failOnError: true,
+        },
+      },
+      vite: { experimental: { supportAnalogFormat: true } },
+    }),
+    nxViteTsPaths(),
+  ] as PluginOption[],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-setup.ts'],
+    include: ['**/*.spec.ts'],
+    reporters: ['default'],
+  },
+  define: {
+    'import.meta.vitest': mode !== 'production',
+  },
+}));
