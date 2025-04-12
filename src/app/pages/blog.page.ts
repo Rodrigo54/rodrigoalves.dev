b7b1c608de6baf99e3b4fbc01f747db5d599416c
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-04-12 12:48:24 -0300

    ✨ feat(app): migrate app-root and blog page to Angular components

---
+++ src/app/pages/blog.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import LayoutComponent from '@app/components/layout.ng';
+
+@Component({
+  selector: 'app-blog-page',
+  imports: [LayoutComponent, RouterOutlet],
+  template: `
+    <layout>
+      <router-outlet></router-outlet>
+    </layout>
+  `,
+  styles: [`
+    :host {
+      background-color: oklab(from var(--color1) 0.1 a b);
+    }
+  `]
+})
+export default class BlogPage {}
--- src/main.server.ts
+++ src/main.server.ts
@@ -2,7 +2,7 @@ import 'zone.js/node';
 import '@angular/platform-server/init';
 import { render } from '@analogjs/router/server';
 
-import App from './app/app-root.ag';
+import AppRoot from './app/app-root.ng';
 import { config } from './app/app.config.server';
 
-export default render(App, config);
+export default render(AppRoot, config);
