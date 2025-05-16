c184e4ae7e591a16fc6f94d9e93bf4e1ae7d7a92
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-05-16 17:39:48 -0300

    ⚙️ config(app): reorganize imports and ensure consistent provider order

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,6 @@
+import { provideContent, withMarkdownRenderer } from '@analogjs/content';
+import { withShikiHighlighter } from '@analogjs/content/shiki-highlighter';
+import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
 import {
   provideHttpClient,
   withFetch,
@@ -5,19 +8,16 @@ import {
 } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
-import { provideContent, withMarkdownRenderer } from '@analogjs/content';
-import { withShikiHighlighter } from '@analogjs/content/shiki-highlighter';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideFileRouter(),
     provideHttpClient(
       withFetch(),
       withInterceptors([requestContextInterceptor])
     ),
     provideClientHydration(),
     provideContent(withMarkdownRenderer(), withShikiHighlighter()),
+    provideFileRouter(),
   ],
 };
