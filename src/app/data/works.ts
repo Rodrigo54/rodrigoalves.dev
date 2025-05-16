56609974426aa38d147d9606bf755febb874f14b
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-05-16 17:36:55 -0300

    ✨ feat(data): add social links  and work experience data structure

---
+++ src/app/data/works.ts
@@ -0,0 +1,71 @@
+export const Works = [
+  {
+    position: 'Desenvolvedor Full-Stack PHP',
+    locale: 'Escritório Escola',
+    duration: {
+      init: '2014-07-01',
+      end: '2017-07-01',
+    },
+    description:
+      'O escritório escola é uma proposta de ensino complementar disponibilizada aos alunos e direcionada para o exercício prático da profissão nos dias atuais. Esta metodologia foi adotada porque tem como base o resgate dos "conteúdos vivos", que realmente estão de acordo com a realidade do aluno e a necessidade do mercado, carente por profissionais devidamente qualificados e atualizados com as perspectivas, cada vez mais arrojadas e urgentes, dos processos de tomada de decisão. Inicialmente o escritório escola trabalha com as rotinas dos escritórios de serviços com atividades mais especializadas, como: assessorias, planejamento, desenvolvimento, análise e demais atividades a fins.',
+  },
+  {
+    position: 'Auxiliar de CPD',
+    locale: 'Grupo Mateus',
+    duration: {
+      init: '2013-05-01',
+      end: '2014-06-01',
+    },
+    description:
+      ' Suporte técnico aos usuários do sistema, Administração da Rede de informática interna, Instalação e manutenção de computadores e servidores, Atualizações de Programas, Manuseio de Impressoras, Gerenciamento de Sistemas de informação (Visual Stores e Suporteware).',
+  },
+  {
+    position: 'Dev Full-Stack JS',
+    locale: 'Startup Cevitech',
+    duration: {
+      init: '2017-09-01',
+      end: '2018-08-01',
+    },
+    description:
+      ' Desenvolvimento de um software voltado para o mercado de hotelaria e motelaria, numa equipe pequena utilizando tecnologias como Angular, Typescript, Rxjs, GraphQL, Firebase.',
+  },
+  {
+    position: 'Dev Full-Stack JS e Mobile',
+    locale: 'ATAC Sistemas',
+    duration: {
+      init: '2018-11-01',
+      end: '2020-01-01',
+    },
+    description:
+      ' Desenvolvimento de aplicações Mobile (Android e iOS) com Ionic e desenvolvimento de backend de integração com um software ERP de emissão de notas fiscais utilizando tecnologias como Nodejs, Express, MSSQL.',
+  },
+  {
+    position: 'Dev Full-Stack JS e Mobile',
+    locale: 'Celero Automação Financeira',
+    duration: {
+      init: '2020-03-01',
+      end: '2021-06-01',
+    },
+    description:
+      'Atuei no desenvolvimento de soluções financeiras para micro e pequenas empresas, construindo um serviço completo de gestão financeira em plataforma web e mobile. Utilizamos Angular, React Native, NestJS e Django. O sucesso do projeto conquistou investimentos do Google e Santander. No processo adquiri experiência em startups, com foco em trabalho em equipe e agilidade.',
+  },
+  {
+    position: 'Dev Full-Stack JS',
+    locale: 'Banco Sicoob',
+    duration: {
+      init: '2021-07-01',
+      end: '2021-11-01',
+    },
+    description:
+      'No shopcoopera.com.br fui responsável por melhorias e manutenção, focando na otimização das integrações do sistema com parceiros do marketplace. Utilizamos o Next.js no e-commerce, resultando em maior segurança nas transações e melhor integração entre parceiros.',
+  },
+  {
+    position: 'Desenvolvedor Frontend e Mobile',
+    locale: 'Senior Sistemas - Softwares para logística ',
+    duration: {
+      init: '2021-11-15',
+    },
+    description:
+      'Atuei em um desafio de integração entre diferentes soluções Logísticas. Desenvolvemos um software de BI abrangente usando Angular e dotnet, o que resultou em maior eficiência e satisfação dos clientes. Essa experiência me permitiu aprimorar minhas habilidades com Angular.',
+  },
+];
