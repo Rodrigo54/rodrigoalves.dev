9fffe1a4a92f868dfe8940a9d18d68afd6f00192
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-06-10 12:56:42 -0300

    💄 style(paper-layout): refactor styles for improved readability and consistency

--- src/app/shared/paper-layout.ng.ts
+++ src/app/shared/paper-layout.ng.ts
@@ -15,66 +15,68 @@ import { Component, input } from '@angular/core';
       </section>
     </div>
   `,
-  styles: [`
-    :host {
-      display: flex;
-      flex-flow: column nowrap;
-      position: relative;
-      align-items: center;
-    }
-    .thumbnail {
-      z-index: 1;
-      width: 100%;
-      height: 450px;
-      overflow: hidden;
-      position: relative;
-      img {
-        aspect-ratio: 16 / 9;
-        object-fit: cover;
-        position: absolute;
-        height: 100%;
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-flow: column nowrap;
+        position: relative;
+        align-items: center;
+      }
+      .thumbnail {
+        z-index: 1;
         width: 100%;
-        inset: 0px;
+        height: 450px;
+        overflow: hidden;
+        position: relative;
+        img {
+          aspect-ratio: 16 / 9;
+          object-fit: cover;
+          position: absolute;
+          height: 100%;
+          width: 100%;
+          inset: 0px;
+        }
       }
-    }
-    .paper-box {
-      z-index: 2;
-      margin: -200px auto 0px;
-      max-width: 1200px;
-      width: 90%;
-      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.1), 0 10px 10px rgba(0, 0, 0, 0.1);
-    }
-    .paper-header {
-      display: flex;
-      flex-flow: column nowrap;
-      align-items: flex-start;
-      justify-content: center;
-      gap: 1.4rem;
-      color: var(--color2-contrast);
-      background: var(--color2);
-      padding: 25px;
-      padding-bottom: 0px;
-      h1 {
-        font-family: var(--font-serif);
-        font-weight: 800;
-        letter-spacing: 0.1rem;
-        line-height: 1.4;
-        word-break: break-word;
-        font-size: 2.2rem;
+      .paper-box {
+        z-index: 2;
+        margin: -200px auto 0px;
+        max-width: 1200px;
+        width: 90%;
+        box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+        border-radius: 6px;
+        background: var(--color2);
       }
-      p {
-        font-family: var(--font-sans-serif);
-        line-height: 1.4;
-        font-size: 1.5rem;
-        font-weight: 300;
+      .paper-header {
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: flex-start;
+        justify-content: center;
+        gap: 1.4rem;
+        color: var(--color2-contrast);
+        padding: 25px;
+        padding-bottom: 0px;
+        h1 {
+          font-family: var(--font-serif);
+          font-weight: 800;
+          letter-spacing: 0.1rem;
+          line-height: 1.4;
+          word-break: break-word;
+          font-size: 2.2rem;
+        }
+        p {
+          font-family: var(--font-sans-serif);
+          line-height: 1.4;
+          font-size: 1.5rem;
+          font-weight: 300;
+        }
       }
-    }
-    .paper-content {
-      color: var(--color2-contrast);
-      background: var(--color2);
-      padding: 25px;
-    }
-  `],
+      .paper-content {
+        color: var(--color2-contrast);
+        padding: 25px;
+      }
+    `,
+  ],
 })
 export default class PaperLayout {
   alt = input('Featured Image');
