246f8a93f552d1934a4392c47316c644b6d721eb
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-06-10 12:56:36 -0300

    💄 style(pagination): clean up template and improve formatting

--- src/app/shared/pagination.ng.ts
+++ src/app/shared/pagination.ng.ts
@@ -5,83 +5,90 @@ import { matArrowBackIosOutline, matArrowForwardIosOutline } from '@ng-icons/mat
 
 @Component({
   selector: 'pagination',
-    imports: [NgIcon, RouterLink],
-    providers: [
-      provideIcons({
-        matArrowBackIosOutline,
-        matArrowForwardIosOutline
-      }),
-    ],
+  imports: [NgIcon, RouterLink],
+  providers: [
+    provideIcons({
+      matArrowBackIosOutline,
+      matArrowForwardIosOutline,
+    }),
+  ],
   template: `
     <div class="pagination-prev">
       @if (showPrev()) {
-        <a [routerLink]="['.']" [queryParams]="{ page: previousPage() }">
-          <ng-icon name="matArrowBackIosOutline" />
-          <span>Página Anterior</span>
-        </a>
+      <a [routerLink]="['.']" [queryParams]="{ page: previousPage() }">
+        <ng-icon name="matArrowBackIosOutline" />
+        <span>Página Anterior</span>
+      </a>
       }
     </div>
-    <div class="pagination-info" >
-      <p>Página {{ page() }} de {{ total() }}</p>
+    <div class="pagination-info">
+      <p><span>Página</span> {{ page() }} de {{ total() }}</p>
     </div>
     <div class="pagination-next">
       @if (showNext()) {
-        <a [routerLink]="['.']" [queryParams]="{ page: nextPage() }">
-          <span>Próxima Página</span>
-          <ng-icon name="matArrowForwardIosOutline" />
-        </a>
+      <a [routerLink]="['.']" [queryParams]="{ page: nextPage() }">
+        <span>Próxima Página</span>
+        <ng-icon name="matArrowForwardIosOutline" />
+      </a>
       }
     </div>
   `,
-  styles: [`
-    :host {
-      border-top: 1px solid var(--color1-light);
-      margin-top: auto;
-      padding: 1.5rem 3rem;
-      color: var(--color2-contrast);
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr;
-      grid-template-rows: 1fr;
-      a {
-        display: flex;
-        flex-flow: row nowrap;
-        align-items: center;
+  styles: [
+    `
+      :host {
+        border-top: 1px solid var(--color1-light);
+        margin-top: auto;
+        padding: 1.5rem 3rem;
         color: var(--color2-contrast);
-        text-decoration: none;
-        transition: color 0.5s;
-        &:hover {
-          color: var(--color3-light);
-        }
-        span {
-          margin: 0px 10px;
-        }
-      }
-      .pagination-next{
-        justify-self: end;
-      }
-      .pagination-prev{
-        justify-self: start;
-      }
-      .pagination-info {
-        justify-self: center;
-        text-align: center;
-        p {
-          margin: 0px;
-          font-size: 1rem;
-        }
-      }
-      @media (max-width: 1170px) {
-        padding: 1rem 2rem;
-        font-size: 0.8rem;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr;
         a {
+          display: flex;
+          flex-flow: row nowrap;
+          align-items: center;
+          color: var(--color2-contrast);
+          text-decoration: none;
+          transition: color 0.5s;
+          &:hover {
+            color: var(--color3-light);
+          }
           span {
-            display: none;
-            visibility: hidden;
+            margin: 0px 10px;
+          }
+        }
+        .pagination-next {
+          justify-self: end;
+        }
+        .pagination-prev {
+          justify-self: start;
+        }
+        .pagination-info {
+          justify-self: center;
+          text-align: center;
+          p {
+            margin: 0px;
+            font-size: 1rem;
+            @media (max-width: 450px) {
+              span {
+                display: none;
+              }
+            }
+          }
+        }
+        @media (max-width: 1170px) {
+          padding: 1rem 2rem;
+          font-size: 0.8rem;
+          a {
+            span {
+              display: none;
+              visibility: hidden;
+            }
           }
         }
       }
-    }
-  `]
+    `,
+  ],
 })
 export class Pagination {
   page = input(1);
