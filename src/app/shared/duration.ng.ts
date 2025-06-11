80d42145a3a3626bd104c3c9c926bcce9948f78c
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-06-11 19:20:36 -0300

    ✨ feat(duration.component): enhance date formatting and add duration display

--- src/app/shared/duration.ng.ts
+++ src/app/shared/duration.ng.ts
@@ -1,34 +1,61 @@
 import { Component, computed, input } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { matCalendarMonth } from '@ng-icons/material-icons/baseline';
-import { formatDistance, intlFormat } from 'date-fns';
+import { formatDuration, intervalToDuration } from 'date-fns';
 import { ptBR } from 'date-fns/locale/pt-BR';
 import _ from 'lodash';
 
-const formatData = (data: string) =>
-  _.capitalize(
-    intlFormat(new Date(data), {
-      month: 'long',
-      year: 'numeric',
-      timeZone: 'UTC',
-    })
-  );
+const formatData = (data: string | Date, type: 'full' | 'short') => {
+  const dateFormat = new Intl.DateTimeFormat('pt-BR', {
+    month: type === 'full' ? 'long' : '2-digit',
+    year: 'numeric',
+    timeZone: 'UTC',
+  });
+  if (typeof data === 'string') {
+    data = new Date(data);
+  }
+  if (isNaN(data.getTime())) {
+    return 'Data inválida';
+  }
+  return _.capitalize(dateFormat.format(data));
+};
+
+const durationFormat = ({ init, end }: { init: string | Date; end: string | Date }) => {
+  const duration = intervalToDuration({
+    start: init instanceof Date ? init : new Date(init),
+    end: end instanceof Date ? end : new Date(end),
+  });
+  return formatDuration(duration, {
+    format: ['years', 'months'],
+    locale: ptBR,
+  });
+};
 
 @Component({
   selector: 'duration',
   imports: [NgIcon],
   template: `
-    <p>
+    <p class="full-date">
       <ng-icon name="matCalendarMonth" size="20" />
       <time [dateTime]="init()">
-        {{ formattedInit() }}
+        {{ fullInit() }}
       </time>
       -
       <time [dateTime]="end()">
-        {{ formattedEnd() }}
+        {{ fullEnd() }}
       </time>
       •
-      <span>{{ timeElapsed() }}</span>
+      <span>{{ fullTimeElapsed() }}</span>
+    </p>
+    <p class="short-date">
+      <ng-icon name="matCalendarMonth" size="20" />
+      <time [dateTime]="init()">
+        {{ shortInit() }}
+      </time>
+      -
+      <time [dateTime]="end()">
+        {{ shortEnd() }}
+      </time>
     </p>
   `,
   providers: [
@@ -38,16 +65,42 @@ const formatData = (data: string) =>
   ],
   styles: [
     `
-      p {
-        display: flex;
-        flex-flow: row nowrap;
-        align-items: center;
-        gap: 10px;
-
-        line-height: 1;
-        font-weight: 200;
-        font-size: 1.1rem;
+      :host {
+        display: block;
+        color: var(--text-color);
+        font-family: var(--font-sans);
         margin: 0px;
+        container-type: inline-size;
+        width: 100%;
+        p {
+          display: flex;
+          flex-flow: row nowrap;
+          align-items: center;
+          gap: 10px;
+
+          line-height: 1;
+          font-weight: 200;
+          font-size: 1.1rem;
+          margin: 0px;
+        }
+
+        @container (width < 525px) {
+          p.full-date {
+            display: none;
+          }
+          p.short-date {
+            display: flex;
+          }
+        }
+
+        @container (width >= 525px) {
+          p.full-date {
+            display: flex;
+          }
+          p.short-date {
+            display: none;
+          }
+        }
       }
     `,
   ],
@@ -56,16 +109,27 @@ export default class DurationComponent {
   init = input.required<string>();
   end = input<string>();
 
-  formattedInit = computed(() => formatData(this.init()));
-  formattedEnd = computed(() => {
-    const endDate = this.end();
-    return endDate ? formatData(endDate) : 'Até o momento';
+  fullInit = computed(() => {
+    const init = this.init();
+    return formatData(init, 'full');
   });
-  timeElapsed = computed(() => {
+
+  fullEnd = computed(() => {
+    const endDate = this.end();
+    return endDate ? formatData(endDate, 'full') : 'Até o momento';
+  });
+  fullTimeElapsed = computed(() => {
     const init = this.init();
     const end = this.end() ?? new Date();
-    return formatDistance(init, end, {
-      locale: ptBR,
-    });
+    return durationFormat({ init, end });
+  });
+
+  shortInit = computed(() => {
+    const init = this.init();
+    return formatData(init, 'short');
+  });
+  shortEnd = computed(() => {
+    const end = this.end();
+    return end ? formatData(end, 'short') : 'Atualmente';
   });
 }
