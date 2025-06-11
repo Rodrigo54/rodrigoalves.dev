c0313798b4438e43260e2b9b43676131557153db
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-06-11 19:21:24 -0300

    💄 style(social-links.ng.ts): enhance layout and styling for social links display

--- src/app/shared/social-links.ng.ts
+++ src/app/shared/social-links.ng.ts
@@ -22,13 +22,16 @@ import { faSolidChessBoard } from '@ng-icons/font-awesome/solid';
 @Component({
   selector: 'social-links',
   imports: [NgIcon],
+  host: {
+    '[class.show-all]': 'showAll()',
+  },
   template: `
     @for (icon of socialLinksList(); track icon.url) { @if (!showAll() && icon.show) {
     <a [href]="icon.url" target="_blank" rel="noopener noreferrer">
       <ng-icon [name]="icon.icon" />
     </a>
     } } @if (showAll()) {
-    <div>
+    <div class="social-links">
       <h2>Redes Sociais</h2>
       <ul>
         @for (icon of socialLinksList(); track icon.url) {
@@ -52,26 +55,55 @@ import { faSolidChessBoard } from '@ng-icons/font-awesome/solid';
         display: flex;
         gap: 1rem;
         color: var(--text-color);
+        container-type: inline-size;
+        width: 100%;
+        a {
+          color: var(--text-color);
+          transition: color 0.5s;
+          --ng-icon__size: 2rem;
+          &:hover {
+            color: var(--primary-color);
+          }
+        }
       }
-      a {
-        color: var(--text-color);
-        transition: color 0.5s;
-        --ng-icon__size: 2rem;
-      }
-      a:hover {
-        color: var(--primary-color);
-      }
-      ul {
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        grid-template-rows: repeat(3, 1fr);
-        gap: 22px 18px;
-        li {
-          a {
-            display: flex;
-            flex-flow: row nowrap;
-            align-items: center;
-            gap: 12px;
+
+      :host(.show-all) {
+        display: block;
+        width: 100%;
+        .social-links {
+          display: block;
+          width: 100%;
+          ul {
+            display: grid;
+            grid-template-columns: repeat(5, 1fr);
+            grid-template-rows: repeat(3, 1fr);
+            gap: 22px 18px;
+            li {
+              a {
+                display: flex;
+                flex-flow: row nowrap;
+                align-items: center;
+                gap: 12px;
+                div {
+                  line-height: 1.2;
+                  span {
+                    font-size: 0.9rem;
+                  }
+                }
+              }
+            }
+          }
+          @container (width < 1150px) {
+            ul {
+              grid-template-columns: repeat(3, 1fr);
+              grid-template-rows: repeat(4, 1fr);
+            }
+          }
+          @container (width < 630px) {
+            ul {
+              grid-template-columns: repeat(1, 1fr);
+              grid-template-rows: repeat(auto-fill, 1fr);
+            }
           }
         }
       }
