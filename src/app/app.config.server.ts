51961aced028c82bb230ec23ae6b024e38eaacaf
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-06-11 15:50:57 -0300

    ✨ feat(app): integrate service worker and update manifest for PWA support

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,9 +1,19 @@
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/ssr';
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideLocalStorageImpl } from 'ngxtension/inject-local-storage';
 import { appConfig } from './app.config';
 
+const localStoreMock = {
+  getItem: (key: string) => null,
+  setItem: (key: string, value: string) => {},
+  removeItem: (key: string) => {},
+  clear: () => {},
+  key: (index: number) => null,
+  length: 0,
+};
+
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()],
+  providers: [provideServerRendering(), provideLocalStorageImpl(localStoreMock)],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,26 +1,21 @@
 import { provideContent, withMarkdownRenderer } from '@analogjs/content';
 import { withShikiHighlighter } from '@analogjs/content/shiki-highlighter';
 import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
-import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from '@angular/common/http';
-import {
-  ApplicationConfig,
-  provideZonelessChangeDetection,
-} from '@angular/core';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, isDevMode, provideZonelessChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([requestContextInterceptor])
-    ),
+    provideHttpClient(withFetch(), withInterceptors([requestContextInterceptor])),
     provideClientHydration(),
     provideContent(withMarkdownRenderer(), withShikiHighlighter()),
     provideFileRouter(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
