400ab59cd94557ab1f916e32f5b5600a7910e5ca
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2025-06-15 21:12:16 -0300

    🛠️ chore(app.config): enable incremental hydration for client hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,14 @@ import { withShikiHighlighter } from '@analogjs/content/shiki-highlighter';
 import { provideFileRouter, requestContextInterceptor } from '@analogjs/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, isDevMode, provideZonelessChangeDetection } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withIncrementalHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideHttpClient(withFetch(), withInterceptors([requestContextInterceptor])),
-    provideClientHydration(),
+    provideClientHydration(withIncrementalHydration()),
     provideContent(withMarkdownRenderer(), withShikiHighlighter()),
     provideFileRouter(),
     provideServiceWorker('ngsw-worker.js', {
